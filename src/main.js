c4bad8950b45aa96788413ed317f2c5e612f78d1
doantu05150 <doantufptu@gmail.com>
2020-02-23 18:54:09 +0700

    add tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import tailwind into vue app
+import '@/assets/css/tailwind.css'
+
+import App from './App'
 
 // import router to vue instance
 import Routers from './router'
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <p>this is home page</p>
+    <div class="text-gray-800 text-2xl">this is home page</div>
   </div>
 </template>
