0f7bf5666e012aee2fd225fbc3bc717c13525c1d
doantu05150 <doantufptu@gmail.com>
2020-03-16 14:39:59 +0700

    fix lf

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="container-fluid h-screen">
       <t-navbar />
-      <div class="flex content-center justify-center bg-gray-500">
+      <div class="flex content-center justify-center bg-gray-500 h-full">
         <div class="mx-auto pt-20">
           <t-form />
         </div>
@@ -13,8 +13,13 @@
 
 <script>
 import { TNavbar, TFormWrapper } from '@/components/common'
-
+ 
 export default {
+  data() {
+    return {
+
+    }
+  },
   components: {
     't-navbar': TNavbar,
     't-form': TFormWrapper
